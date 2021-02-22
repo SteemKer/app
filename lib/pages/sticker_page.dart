5a989fa7a8607edb8081661c0f5d2c8421e84858
Piyush Bhangale <bhangalepiyush@gmail.com>
2021-02-22 15:10:54 +0530

    prototype sticker page

--- lib/pages/sticker_page.dart
+++ lib/pages/sticker_page.dart
@@ -16,6 +16,7 @@ class StickerPage extends StatefulWidget {
 class _StickerPage extends State<StickerPage> {
   final storage = new FlutterSecureStorage();
   List<dynamic> data = [];
+  List<Widget> cards = [];
 
   Future isLoggedIn() async {
     final token = await storage.read(key: "token");
@@ -41,9 +42,43 @@ class _StickerPage extends State<StickerPage> {
     }
 
     List<dynamic> decodedData = jsonDecode(response.body);
+    List<Widget> _cards = [];
+    decodedData.forEach((pack) {
+      _cards.add(Container(
+        height: 200,
+        width: (MediaQuery.of(context).size.width -
+            20 * MediaQuery.of(context).size.width / 100),
+        padding: EdgeInsets.all(4),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(8),
+            ),
+          ),
+          color: Colors.white12,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                pack["name"],
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  onAddPressed(pack["pack_id"]);
+                },
+                child: Text("Add"),
+              ),
+            ],
+          ),
+        ),
+      ));
+    });
 
     setState(() {
       data = decodedData;
+      cards = _cards;
     });
   }
 
@@ -54,11 +89,23 @@ class _StickerPage extends State<StickerPage> {
     isLoggedIn().then((isLogged) => null);
   }
 
+  // ignore: missing_return
+  onAddPressed(packID) {
+    print(packID);
+    print("pressed");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Center(
-        child: Text("hi"),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: cards,
+          ),
+        ),
       ),
     );
   }

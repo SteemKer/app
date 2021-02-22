b71244d9e1f87134448121fec1943fc8b0f09148
Piyush Bhangale <bhangalepiyush@gmail.com>
2021-02-22 19:12:40 +0530

    loading screen and conditional progressIndicator

--- lib/pages/sticker_page.dart
+++ lib/pages/sticker_page.dart
@@ -1,9 +1,15 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_conditional_rendering/conditional.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
+import 'package:loading_overlay/loading_overlay.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:whatsapp_stickers/whatsapp_stickers.dart';
 
 import 'login_page.dart';
 
@@ -16,6 +22,11 @@ class StickerPage extends StatefulWidget {
 
 class _StickerPage extends State<StickerPage> {
   final storage = new FlutterSecureStorage();
+  final dio = Dio();
+  int downloaded = 1;
+  int toDownload = 2;
+  bool _isLoading = true;
+  bool screenLoaded = false;
   List<dynamic> data = [];
   List<Widget> cards = [];
 
@@ -82,6 +93,7 @@ class _StickerPage extends State<StickerPage> {
     setState(() {
       data = decodedData;
       cards = _cards;
+      _isLoading = false;
     });
   }
 
@@ -121,19 +133,72 @@ class _StickerPage extends State<StickerPage> {
     }
   }
 
-  Future downloadFromServer() async {
-    var applicationsDocumentDirectory = await getApplicationDocumentsDirectory();
+  Future downloadFromServer(
+    String packID,
+    Map<String, dynamic> packData,
+  ) async {
+    var applicationsDocumentDirectory =
+        await getApplicationDocumentsDirectory();
+    var stickersDirectory =
+        Directory("${applicationsDocumentDirectory.path}/stickers/$packID");
+    bool exists = await stickersDirectory.exists();
+    if (exists) {
+      await stickersDirectory.delete(recursive: true);
+    }
+    await stickersDirectory.create(recursive: true);
+    final downloads = <Future>[];
+
+    packData["data"].forEach(
+      (emoteData) {
+        downloads.add(
+          dio.download(emoteData["url"],
+              "${stickersDirectory.path}/${emoteData["name"]}.webp"),
+        );
+      },
+    );
+
+    await Future.wait(downloads);
+
+    var stickerPack = WhatsappStickers(
+      identifier: packID,
+      name: packData["name"],
+      publisher: 'Steeker',
+      trayImageFileName: WhatsappStickerImage.fromAsset('assets/logo.png'),
+      publisherWebsite: 'https://steeker.piyushdev.ml',
+      privacyPolicyWebsite: 'https://steeker.piyushdev.ml/privacy',
+      licenseAgreementWebsite: 'https://steeker.piyushdev.ml/license',
+    );
+
+    packData["data"].forEach(
+      (emoteData) {
+        stickerPack.addSticker(
+          WhatsappStickerImage.fromFile(
+              "${stickersDirectory.path}/${emoteData["name"]}.webp"),
+          ["😉"],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: cards,
+    return LoadingOverlay(
+      isLoading: _isLoading,
+      progressIndicator: Conditional.single(
+          context: context,
+          conditionBuilder: (BuildContext ctx) => screenLoaded,
+          widgetBuilder: (BuildContext ctx) => CircularProgressIndicator(
+                value: downloaded / toDownload,
+              ),
+          fallbackBuilder: (BuildContext ctx) => CircularProgressIndicator()),
+      child: Container(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: cards,
+            ),
           ),
         ),
       ),

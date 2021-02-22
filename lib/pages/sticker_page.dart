31ee3ce9f1d71fc64ea627457ab11152358aab05
Piyush Bhangale <bhangalepiyush@gmail.com>
2021-02-22 13:44:32 +0530

    raw sticker screen

---
+++ lib/pages/sticker_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StickerPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _StickerPage();
+  }
+}
+
+class _StickerPage extends State<StickerPage> {
+  @override
+  Widget build(BuildContext context) {
+  }
+  
+}

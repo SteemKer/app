8e48f997b7c8d6cc6ac2e59a7e077684d2691072
Piyush Bhangale <bhangalepiyush@gmail.com>
2021-02-23 10:40:18 +0530

    main: initialize sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:steeker/pages/landing_page.dart';
 
-void main() {
-  runApp(MyApp());
+Future<void> main() async {
+  await SentryFlutter.init(
+        (options) {
+      options.dsn = 'https://eb74916782e146d698b22a6e9d06193a@o170856.ingest.sentry.io/5648249';
+    },
+    appRunner: () => runApp(SteekerApp()),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class SteekerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

3ee3b5aa9fc9d304b6cf3a1696290ad0fcea14cf
Piyush Bhangale <bhangalepiyush@gmail.com>
2021-02-24 14:51:51 +0530

    add analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:steeker/pages/landing_page.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   await SentryFlutter.init(
-        (options) {
-      options.dsn = 'https://eb74916782e146d698b22a6e9d06193a@o170856.ingest.sentry.io/5648249';
+    (options) {
+      options.dsn =
+          'https://eb74916782e146d698b22a6e9d06193a@o170856.ingest.sentry.io/5648249';
     },
     appRunner: () => runApp(SteekerApp()),
   );
 }
 
 class SteekerApp extends StatelessWidget {
+  final FirebaseAnalytics analytics = FirebaseAnalytics();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,6 +29,9 @@ class SteekerApp extends StatelessWidget {
         primarySwatch: Colors.deepPurple,
       ),
       home: LandingPage(),
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
     );
   }
 }
